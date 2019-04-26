ec4248ea7278694cd5428a56746c5894ee87664f
Remzy4God <remzy222@yahoo.com>
2019-04-26 12:42:41 +0100

    Update db.php

--- database/db.php
+++ database/db.php
@@ -1,5 +1,5 @@
 <?php
-$connect=mysqli_connect("localhost","root","","search-engine");
+$connect=mysqli_connect("www.db4free.net","searchengine2000","searchengine2000","searchengine2000");
 if(!$connect){
 	echo "Failed to Connnect to Database";
 }
